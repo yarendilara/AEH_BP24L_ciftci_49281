a03e2432346a60eb68f249bce145e61b432b0166
yarendilara <yarendilara04@gmail.com>
2025-04-15 00:38:09 +0300

    myFirstApp.java

--- HelloBP.java
+++ HelloBP.java
@@ -1,6 +1,6 @@
-public class HelloBP {
+package pl.pp;
+public class myFirstApp{
     public static void main(String[] args) {
         System.out.println("Hello Basics of Programming!");
     }
 }
-
